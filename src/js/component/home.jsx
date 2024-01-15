59e5ea072f1700b663ec118188e86919f75c62d6
Intetsu0396 <141952373+Intetsu0396@users.noreply.github.com>
2024-01-15 02:49:27 +0000

    finished-project

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,21 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+
+const Home = ({ uno, dos, tres, cuatro, cinco, seis }) => {
+  return (
+    <div className="allCounter">
+      <div className="clock box">
+        <i class="bi bi-clock-fill"></i>
+      </div>
+      <div className="box">{seis}</div>
+      <div className="box">{cinco}</div>
+      <div className="box">{cuatro}</div>
+      <div className="box">{tres}</div>
+      <div className="box">{dos}</div>
+      <div className="box">{uno}</div>
+    </div>
+  );
 };
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,27 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
-
+function simpleCounter(props) {}
+let counter = 0;
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+setInterval(() => {
+  counter += 1;
+  let primerNumero = Math.floor((counter / 1) % 10);
+  let segundoNumero = Math.floor((counter / 10) % 10);
+  let tercerNumero = Math.floor((counter / 100) % 10);
+  let cuartoNumero = Math.floor((counter / 1000) % 10);
+  let quintoNumero = Math.floor((counter / 10000) % 10);
+  let sextoNumero = Math.floor((counter / 100000) % 10);
+  console.log("primer numero", Math.floor(counter / 1) % 10);
+  ReactDOM.render(
+    <Home
+      seis={sextoNumero}
+      cinco={quintoNumero}
+      cuatro={cuartoNumero}
+      tres={tercerNumero}
+      dos={segundoNumero}
+      uno={primerNumero}
+    />,
+    document.querySelector("#app")
+  );
+}, 1000);
